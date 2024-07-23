8bc8e17effaf3ba412337ad276597d58d9dd83e5
Mohanad Abugharbia <mgharbieh.01@gmail.com>
2024-07-23 16:52:43 +0200

    [CLEANUP]: Cleaned up old hackerbot test code

--- test/memory-test.py
+++
@@ -1,6 +0,0 @@
-from langchain.memory import ConversationBufferMemory
-memory = ConversationBufferMemory()
-memory.chat_memory.add_user_message("hi!")
-memory.chat_memory.add_ai_message("whats up?")
-
-print(memory.load_memory_variables({}))
